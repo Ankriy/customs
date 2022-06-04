0a3ffb3151c9afb389a5c295bab3e2e25ed22b59
evlan <evlan@LAPTOP-78BO0NKA>
2022-06-04 11:58:30 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace customs
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<Tables> result = new List<Tables>(3);
+
+            result.Add(new Tables("0001", "Живая Лошадь", "97%"));
+
+            grid.ItemsSource = result;
+
+        }
+        private void ExitButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();   
+        }
+        private void MinButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+        private void ToolBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+        private void ButtonCkick_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            string fileName = @"C:\Python27\example\hello_world.py";
+
+            Process p = new Process();
+            p.StartInfo = new ProcessStartInfo(@"C:\Python27\python.exe", fileName)
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            p.Start();
+
+            string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+
+            Console.WriteLine(output);
+
+            Console.ReadLine();
+        }
+
+    }
+}
