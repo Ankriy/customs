73c25ab8bf9c481d2d7da9bd35f1c731c4a96a33
lihachev9 <andreyyy.lihachev@gmail.com>
2022-06-05 18:20:10 +0300

    GH

---
+++ Приложение/MainWindow.xaml.cs
@@ -0,0 +1,233 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using Microsoft.Win32;
+
+namespace customs
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Tables> result = new List<Tables>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void loadJson()
+        {
+            using (StreamReader r = new StreamReader("file.json"))
+            {
+                string json = r.ReadToEnd();
+                //List<Item> items = JsonConvert.DeserializeObject<List<Item>>(json);
+            }
+        }
+        private void grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<Tables> result = new List<Tables>();
+
+            //result.Add(new Tables("0001", "Живая Лошадь", "97%"));
+
+            //grid.ItemsSource = result;
+
+        }
+        private void ExitButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+        private void MinButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+        private void ToolBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+        public static string StartClient(string text)
+        {
+            TcpClient clientSocket = new TcpClient();
+            clientSocket.Connect("192.168.90.180", 5000);
+
+            NetworkStream serverStream = clientSocket.GetStream();
+            byte[] outStream = Encoding.UTF8.GetBytes(text + "\u0017" + 3.ToString());
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+
+            byte[] inStream = new byte[10025];
+            serverStream.Read(inStream, 0, inStream.Length);
+            return Encoding.UTF8.GetString(inStream);
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            /////////////////////////////////////////////////////////////////////////////////////////////
+            string text = SearchBox.Text;
+            if (Double.TryParse(text, out double tmp) || String.IsNullOrWhiteSpace(text))
+            {
+                text = text.Trim();
+                if (text.Length < 4)
+                {
+                    for (int i = text.Length; i < 4; i++)
+                        text = "0" + text;
+                }
+                return;
+            }
+
+            string output = StartClient(text);
+
+            result.Clear();
+            grid.ItemsSource = null;
+            grid.DataContext = null;
+            grid.Items.Refresh();
+            if (output == "")
+                MessageBox.Show("Не получилось :/");
+            else
+            {
+                output = output.Replace('"', ' ').Replace("{", "").Replace("}", "").Replace("[", "");
+
+                string[] str = output.Split(',');
+                string[] postStr;
+
+                for (int i = 0; i < str.Length; i++)
+                {
+                    postStr = str[i].Split(':');
+
+                    string n_str = new string(postStr[1].Where(t => char.IsDigit(t)).ToArray());
+
+                    if (n_str[0] != '1')
+                        n_str = n_str.Substring(1, 2);
+                    else
+                        n_str = n_str.Substring(0, 3);
+
+                    string x_str = new string(postStr[0].Where(t => char.IsDigit(t)).ToArray());
+
+                    while (x_str.Length < 4)
+                        x_str = "0" + x_str;
+
+                    ///..........................................................
+                    if (!(int.Parse(n_str) < 1))
+                        result.Add(new Tables(x_str, SearchBox.Text, n_str + "%"));
+                    ///..........................................................
+                }
+                grid.ItemsSource = result;
+            }
+        }
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            string filename = ofd.FileName;
+            string[] fileText;
+
+            if (!(filename == ""))
+            {
+                grid.ItemsSource = null;
+                grid.DataContext = null;
+                result.Clear();
+                fileText = System.IO.File.ReadAllLines(filename);
+                List<Tables> result4 = new List<Tables>();
+                for (int i = 0; i < fileText.Length; i++)
+                {
+                    filldatatable(fileText[i]);
+
+                    //eventsSearchBox(result4, fileText[i]);
+                }
+                //grid.ItemsSource = result4;
+                MessageBox.Show("Успешно");
+            }
+            else
+                MessageBox.Show("Не получилось прочитать файл");
+        }
+        private void filldatatable(string input_str)
+        {
+            string output = StartClient(input_str);
+
+            //result.Clear();
+            grid.ItemsSource = null;
+            grid.DataContext = null;
+
+            grid.Items.Refresh();
+            if (output == "")
+                MessageBox.Show("Не получилось :/");
+            else
+            {
+                output = output.Replace('"', ' ').Replace("{", "").Replace("}", "").Replace("[", "");
+
+                string[] str = output.Split(',');
+                string[] postStr;
+
+                postStr = str[0].Split(':');
+                try
+                {
+
+                    string n_str = new string(postStr[1].Where(t => char.IsDigit(t)).ToArray());
+                    string x_str = new string(postStr[0].Where(t => char.IsDigit(t)).ToArray());
+
+                    if (n_str[0] != '1')
+                        n_str = n_str.Substring(1, 2);
+                    else
+                        n_str = n_str.Substring(0, 3);
+
+
+
+                    while (x_str.Length < 4)
+                        x_str = "0" + x_str;
+
+
+                    result.Add(new Tables(x_str, input_str, n_str + "%"));
+                    grid.ItemsSource = result;
+                }
+                catch
+                {
+                    MessageBox.Show(postStr[0].ToString());
+                }
+            }
+        }
+
+        private void SearchBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                Button_Click(null, null);
+        }
+
+
+        private void Export_Click(object sender, RoutedEventArgs e)
+        {
+            if (grid.Items.Count == 0)
+                return;
+            // Configure save file dialog box
+            var dialog = new SaveFileDialog();
+            dialog.FileName = "Document"; // Default file name
+            dialog.DefaultExt = ".csv"; // Default file extension
+            dialog.Filter = "Text documents (.csv)|*.csv"; // Filter files by extension
+
+            // Show save file dialog box
+            bool? result = dialog.ShowDialog();
+
+            // Process save file dialog box results
+            if (result == true)
+            {
+                // Save document
+                StringBuilder sb = new StringBuilder();
+                sb.Append("Код,Запрос,Вероятность\r\n");
+                foreach(var item in grid.Items)
+                {
+                    sb.Append(item);
+                }
+                File.WriteAllText("temp.csv", sb.ToString());
+                Clipboard.Clear();
+                StreamWriter file = new StreamWriter(dialog.FileName);
+                file.WriteLine(sb.ToString());
+                file.Close();
+            }
+        }
+    }
+}
--- Сервисы/utils.py
+++ Сервисы/utils.py
@@ -1,5 +1,6 @@
 import json
 import re
+import numpy as np
 from tensorflow.keras.preprocessing.text import tokenizer_from_json
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import load_model
@@ -10,14 +11,12 @@ model = load_model('models/my_model/1')
 
 with open('categories.json', 'r') as f:
     categories = json.load(f)
+reversed_categories = dict((v, int(k)) for k, v in categories.items())
 
 
 with open('tokenizer.json') as f:
     data = json.load(f)
     tokenizer = tokenizer_from_json(data)
-
-
-reversed_categories = dict((v, int(k)) for k, v in categories.items())
 
 
 def clear_text(data):
@@ -31,11 +30,20 @@ def prep_text(texts, tokenizer, max_sequence_length):
     return pad_sequences(text_sequences, maxlen=max_sequence_length, padding='post')
 
 
-def prep_data(text):
+def prep_data_one_str(text):
     data = clear_text(text)
     return prep_text([data], tokenizer, maxSequenceLength)
 
 
 def predict_model(data):
-    data = prep_data(data)
+    data = prep_data_one_str(data)
     return model.predict(data)
+
+
+def dict_predict(predict, elementov):
+    order = np.argsort(predict)
+    result = {}
+    for i in range(elementov):
+        value_index = order[0][-i - 1]
+        result[reversed_categories[value_index]] = predict[0][value_index]
+    return dict((str(k), str(v)) for k, v in result.items())
